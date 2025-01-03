16b07eff7427548f565eeb7c66b6094b21849f66
Dib <79500311+tdib@users.noreply.github.com>
2025-01-03 23:29:46 +1100

    Set window decoration to resize

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,6 +11,7 @@ config.automatically_reload_config = true
 config.macos_window_background_blur = 80
 config.use_fancy_tab_bar = false
 config.font_size = 14
+config.window_decorations = "RESIZE"
 
 -- Disable ligatures
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
